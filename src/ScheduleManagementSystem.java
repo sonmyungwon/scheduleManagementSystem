988217efebe6fe6a8f783e060caa138e778ffb5c
sonmyungwon <45677898+sonmyungwon@users.noreply.github.com>
2022-03-25 22:28:35 +0900

    과제

--- src/ScheduleManagementSystem.java
+++ src/ScheduleManagementSystem.java
@@ -3,68 +3,63 @@ import java.util.Scanner;
 public class ScheduleManagementSystem {
 	public static void main (String args[]) {
 		int com = 0;
-		String scheduleName = null;
-		String scheduleDate= null;
-		String place= null;
-		String friend= null;
-		String pnum= null;
 		Scanner input= new Scanner(System.in);
 		while(com!=6){
-		System.out.println("1. Add Schedule");
-		System.out.println("2. Delate Schedule");
-		System.out.println("3. Edit Schedule");
-		System.out.println("4. View Schedule");
-		System.out.println("5. show a menu");
-		System.out.println("6. Exit");
-		System.out.print("Please enter a number:");
-		com = input.nextInt();
-		switch(com) {
-		case 1 : 		
-			System.out.println("schedule name: ");		
-			scheduleName = input.next();
-			System.out.println("schedule date: ");
-			scheduleDate = input.next();
-			System.out.println("meeting place: ");
-			place = input.next();
-			System.out.println("With whom: ");
-			friend = input.next();
-			System.out.println("contact number: ");
-			pnum = input.next();
-			break;
-		
-		case 2:
-			scheduleName="";
-			scheduleDate="";
-			place="";
-			friend="";
-			pnum="";
-			break;
-		case 3:
-			System.out.println("schedule name: "+scheduleName);
-			System.out.print("change	: ");
-			scheduleName = input.next();
-			System.out.println("schedule date: "+scheduleDate);
-			System.out.print("change	: ");
-			scheduleDate = input.next();
-			System.out.println("meeting place: "+ place);
-			System.out.print("change	: ");
-			place = input.next();
-			System.out.println("With whom: "+friend);
-			System.out.print("change	: ");
-			friend = input.next();
-			System.out.println("contact number: "+pnum);
-			System.out.print("change	: ");
-			pnum = input.next();
-			break;
-		case 4:
-			System.out.println("schedule name:  " + scheduleName);		
-			System.out.println("schedule date:  " + scheduleDate);
-			System.out.println("meeting place:  " + place);
-			System.out.println("With whom:  " + friend);
-			System.out.println("contact number:  " + pnum);
-		
+			System.out.println("1. Add Schedule");
+			System.out.println("2. Delate Schedule");
+			System.out.println("3. Edit Schedule");
+			System.out.println("4. View Schedule");
+			System.out.println("5. show a menu");
+			System.out.println("6. Exit");
+			System.out.print("Please enter a number:");
+			com = input.nextInt();
+			switch(com) {
+			case 1 : 		
+				addschedule();
+				break;
+			case 2:
+				delateschedule();
+				break;
+			case 3:
+				editschedule();
+				break;
+			case 4:
+				viewschedule();
+				break;
+			case 5:
+				break;
+			case 6:
+				break;
+
 			}
 		}
 	}
-	
+	public static void addschedule() {
+		Scanner input= new Scanner(System.in);
+		System.out.print("schedule name: ");		
+		String scheduleName = input.next();
+		System.out.print("schedule date: ");
+		String scheduleDate = input.next();
+		System.out.print("meeting place: ");
+		String place = input.next();
+		System.out.print("With whom: ");
+		String friend = input.next();
+		System.out.print("contact number: ");
+		String pnum = input.next();
+	}
+	public static void delateschedule() {		
+		Scanner input= new Scanner(System.in);
+		System.out.print("schedule name: ");		
+		String scheduleName = input.next();
+	}
+	public static void editschedule() {		
+		Scanner input= new Scanner(System.in);
+		System.out.print("schedule name: ");		
+		String scheduleName = input.next();
+	}
+	public static void viewschedule() {
+		Scanner input= new Scanner(System.in);
+		System.out.print("schedule name: ");		
+		String scheduleName = input.next();
+	}
 }
